0c249162709e8755c5f8b21b230b84194feccc43
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-09 19:51:13 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,9 +2,9 @@ CREATE TABLE bears (
   id integer PRIMARY KEY,
   name TEXT,
   age INTEGER,
-  gender CHAR(1),
+  gender ENUM("M", "F"),
   color TEXT,
   temperament TEXT,
   alive BOOLEAN
-  ;  
+  ;
 )
