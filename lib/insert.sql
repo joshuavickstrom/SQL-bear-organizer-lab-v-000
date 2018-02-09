5b5a906b6d3b813f0c3f4a7462f5af6e8c1f0794
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-09 19:56:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "chill", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); # no name 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); # no name
