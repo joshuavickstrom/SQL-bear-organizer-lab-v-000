17b9cd33418c7ebefbe2d813c7adc0560a852d42
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-09 20:01:30 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "chill", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy",);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "M", "black", "rowdy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "F", "black", "sassy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 22, "M", "brown", "charming", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy");
